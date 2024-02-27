62df271d1b2c9c607cdce895af86aeec47cf9b43
lgpay <7310567+lgpay@users.noreply.github.com>
2024-02-27 17:20:11 +0800

    Update 180day.js

--- 180day.js
+++ 180day.js
@@ -1,11 +1,9 @@
-/*
-cron "0 5 * * *" 180day.js, tag=180计时
-*/
-
-const notify = require('./sendNotify')
+const notify = require('./sendNotify');
+const moment = require('moment');
 
 // 配置
 const startDate = '2024-01-01'; // 设定开始日期，格式：YYYY-MM-DD
+const sendMessage = []; // 存放发送的通知消息
 
 // 获取当前日期
 const currentDate = moment().format('YYYY-MM-DD');
@@ -18,5 +16,8 @@ const remainingDays = 180 - days;
 
 // 如果距离第180天的天数小于等于3天，则发送通知
 if (remainingDays <= 3) {
-  sendMessage.push(还差${remainingDays}天到达第180天`)
+  sendMessage.push(`还差${remainingDays}天到达第180天`);
 }
+
+// 发送通知
+notify.sendNotify("180天计时", sendMessage.join("\n"));
