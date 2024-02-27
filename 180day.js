1ae4734a4bc45d6aeeb6719375834c8492da82ab
lgpay <7310567+lgpay@users.noreply.github.com>
2024-02-27 16:41:09 +0800

    Update 180day.js

--- 180day.js
+++ 180day.js
@@ -1,14 +1,19 @@
+const fs = require('fs');
 const notifyContent = '截至已满180天';
 
 function checkNotify() {
-  const lastNotifyTime = $prefs.valueForKey('lastNotifyTime'); // 获取上次通知的时间戳
+  const lastNotifyTime = fs.existsSync('./180.txt')
+    ? Number(fs.readFileSync('./last180.txt', 'utf8'))
+    : null; // 从文件中读取上次通知的时间戳
+
   const currentTime = Date.now(); // 获取当前的时间戳
 
   if (!lastNotifyTime || currentTime - lastNotifyTime >= 180 * 24 * 60 * 60 * 1000) {
     // 如果上次通知时间不存在，或者距离上次通知已经超过180天，则执行通知操作
-    $notify('青龙面板通知', '', notifyContent);
-    const lastNotifyTime = new Date('2024-01-01 09:00:00').getTime(); // 设置上次通知时间为2024年1月1日9点
-    $prefs.setValueForKey(lastNotifyTime, 'lastNotifyTime'); // 保存上次通知时间作为上次通知时间
+    console.log('发送通知');
+    // 这里可以使用您想要的方式发送通知，如调用其他模块的函数、发送网络请求等
+
+    fs.writeFileSync('./lastNotifyTime.txt', String(currentTime), 'utf8'); // 保存当前时间戳到文件中
   }
 }
 
