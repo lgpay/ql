b04669cfa48996ba52907cc958bc1cdaa40bb73e
lgpay <7310567+lgpay@users.noreply.github.com>
2024-02-28 08:25:22 +0800

    Update and rename 180day.js to giffgaff.js

--- giffgaff.js
+++ giffgaff.js
@@ -1,9 +1,12 @@
+/*
+cron "30 8 * * *" giffgaff.js, tag=giffgaff到期提醒
+*/
+
 const notify = require('./sendNotify');
 const moment = require('moment');
 
 // 配置
 const startDate = '2024-01-01'; // 设定开始日期，格式：YYYY-MM-DD
-const sendMessage = []; // 存放发送的通知消息
 
 // 获取当前日期
 const currentDate = moment().format('YYYY-MM-DD');
@@ -12,10 +15,10 @@ const currentDate = moment().format('YYYY-MM-DD');
 const days = moment(currentDate).diff(moment(startDate), 'days');
 
 // 计算距离第180天还有几天
-const remainingDays = 180 - days;
+const remainingDays = 60 - days;
 
 // 如果距离第180天的天数小于等于3天，则发送通知
 if (remainingDays <= 3) {
-  sendMessage.push(`还差${remainingDays}天到达第180天`);
-  notify.sendNotify("180天计时", sendMessage.join("\n"));
+  const sendMessage = `距离giffgaff到期还有${remainingDays}天，请尽快保号`;
+  notify.sendNotify(sendMessage);
 }
