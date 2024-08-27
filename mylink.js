52cc35acc14b718b14487bbf83ae3806f2802f0c
lgpay <7310567+lgpay@users.noreply.github.com>
2024-08-27 10:53:27 +0800

    Update mylink.js

--- mylink.js
+++ mylink.js
@@ -7,6 +7,7 @@ const notify = require('./sendNotify');
 const moment = require('moment');
 
 // 配置
+const serviceName = 'MyLink'; // 设置服务名称变量
 const expiryDate = '2024-08-30'; // 设定到期日期，格式：YYYY-MM-DD
 const thresholdDays = 3; // 设定小于等于的天数阈值
 
@@ -16,10 +17,13 @@ const currentDate = moment().format('YYYY-MM-DD');
 // 计算距离到期的天数
 const remainingDays = moment(expiryDate).diff(moment(currentDate), 'days');
 
+// 输出距离到期的天数
+console.log(`距离${serviceName}到期还有${remainingDays}天`);
+
 // 如果距离到期的天数小于等于阈值，则发送通知
 if (remainingDays <= thresholdDays) {
-  const sendMessage = `距离MyLink到期还有${remainingDays}天，请尽快充值`;
+  const sendMessage = `距离${serviceName}到期还有${remainingDays}天，请尽快充值`;
 
   // 发送通知
-  notify.sendNotify('MyLink保号提醒', sendMessage);
+  notify.sendNotify(`${serviceName}保号提醒`, sendMessage);
 }
